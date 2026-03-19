dc27adbaf6c699e12d5b2ac15b42b0ce51629ea3
Alx Wrtl <alx@example.com>
2026-03-19 11:11:08 +0100

    fix: remove SSH userKnownHostsFile deprecation and hide menu bar clutter
    - Move userKnownHostsFile to matchBlocks."*" config
    - Add enableDefaultConfig=false to suppress default SSH config
    - Add GitHub known_hosts via home.file
    - Hide keyboard input indicator and Spotlight icon
    - Disable Sound in control center

--- home/ssh.nix
+++ home/ssh.nix
@@ -10,21 +10,23 @@ let
 in
 
 {
+  home.file.".ssh/known_hosts_github" = {
+    text = "github.com ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okWi0dh2l9GKJl\n";
+  };
+
   programs.ssh = {
     enable = true;
+    enableDefaultConfig = false;
 
     includes = [
       "${config.home.homeDirectory}/.colima/ssh_config"
     ];
 
-    knownHosts = {
-      github = {
-        hostNames = [ "github.com" ];
-        publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okWi0dh2l9GKJl";
-      };
-    };
-
     matchBlocks = {
+      "*" = {
+        userKnownHostsFile = "~/.ssh/known_hosts ~/.ssh/known_hosts_github";
+      };
+
       "github.com" = {
         identityFile = "~/.ssh/id_ed25519";
         identitiesOnly = true;
